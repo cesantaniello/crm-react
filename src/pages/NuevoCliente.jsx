5e82b1861c7ad79cb81848b1b383934bfbe24403
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-09-14 17:20:13 +0200

    Add form

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router-dom"
+import Formulario from "../components/Formulario"
+
 function NuevoCliente() {
   const navigate = useNavigate()
   return (
@@ -13,8 +15,15 @@ function NuevoCliente() {
           Volver
         </button>
       </div>
-      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
-        <p>Formulario aquí</p>
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        <form>
+          <Formulario/>
+          <input
+            type="submit"
+            className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+            value="Registar cliente"
+            />
+          </form>
       </div>
     </>
   )

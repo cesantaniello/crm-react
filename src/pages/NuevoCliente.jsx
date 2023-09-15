8f4f5cbde3bea148798772bdbd78b462320775a5
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2023-09-15 11:33:33 +0200

    Make an action for Form

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,8 +1,15 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Form } from "react-router-dom"
 import Formulario from "../components/Formulario"
 
+export function action() {
+  console.log('Submit al formulario...')
+}
+
+
 function NuevoCliente() {
+
   const navigate = useNavigate()
+  
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Nuevo cliente</h1>
@@ -16,14 +23,16 @@ function NuevoCliente() {
         </button>
       </div>
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
-        <form>
+        <Form
+          method="post"
+        >
           <Formulario/>
           <input
             type="submit"
             className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
             value="Registar cliente"
             />
-          </form>
+        </Form>
       </div>
     </>
   )
